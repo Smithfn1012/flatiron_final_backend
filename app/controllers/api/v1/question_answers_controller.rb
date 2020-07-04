85e1fb6b74c5796d243420be91c8342fb2cfbfd2
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-04 18:25:28 -0500

    build out rest of questions_answers controller

--- app/controllers/api/v1/question_answers_controller.rb
+++ app/controllers/api/v1/question_answers_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::QuestionAnswersController < ApplicationController
+    before_action :find_question_answer, only: [:show, :update]
+  
+    def index
+      @question_answers = QuestionAnswer.all
+      render json: @question_answers
+    end
+  
+    def show
+    end
+  
+    def create
+      @question_answer = QuestionAnswer.create(question_answer_params)
+      render json: @question_answer
+    end
+  
+    def update
+      @question_answer.update(question_answer_params)
+      render json: @question_answer
+    end
+  
+    private
+    def find_question_answer
+      @question_answer = QuestionAnswer.find(params[:id])
+    end
+  
+    def question_answer_params
+      params.permit(:question, :fact, :image)
+    end
 end
