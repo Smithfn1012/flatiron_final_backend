852d5ad77460d465c5ac765fda118a692c804ec7
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-04 17:43:41 -0500

    add assocation and validation to animal.rb

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,5 @@
 class Animal < ApplicationRecord
+  belongs_to :biome
+  has_many :comments
+  validates :species, :lifespan, :size, :prey, :predators, :location, :other_info, :biome_id, :image, presence: :true
 end
