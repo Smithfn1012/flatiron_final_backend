78e9dbe6ad9325af88a6513cd4a4357dd2a3dc6a
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-04 17:47:12 -0500

    add associations and validations to plant.rb

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,5 @@
 class Plant < ApplicationRecord
+  belongs_to :biome
+  has_many :comments 
+  validates :species, :size, :predators, :location, :seed_distribution, :other_info, :biome_id, :image, presence: :true
 end
