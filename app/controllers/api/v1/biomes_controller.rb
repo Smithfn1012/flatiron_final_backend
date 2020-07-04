57f7ef86e85c04d3f78b0a92595cf4c210b67333
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-04 18:03:46 -0500

    add def show & create to biomes controller

--- app/controllers/api/v1/biomes_controller.rb
+++ app/controllers/api/v1/biomes_controller.rb
@@ -4,4 +4,12 @@ class Api::V1::BiomesController < ApplicationController
         @biomes = Biome.all
         render json: @biomes
     end
+
+    def show
+    end
+
+    def create
+        @biome = Biome.create(biome_params)
+        render json: @biome
+    end
 end
