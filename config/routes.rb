468993a88fdab1af33ea7ea55a628e7bdd44aceb
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-04 17:52:38 -0500

    update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :choices
-  resources :question_answers
-  resources :biomes
-  resources :plants
-  resources :animals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :animals do
+        resources :comments
+      end
+      resources :plants do
+        resources :comments
+      end
+      resources :biomes
+      resources :question_answers
+      resources :comments
+    end
+  end
 end
+
+
