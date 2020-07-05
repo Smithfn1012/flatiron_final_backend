886438e440e6329f0f941f963cd79f626ec53fcb
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 00:39:44 -0500

    add attributes to plant_serializer

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,3 +1,3 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :species, :size, :predators, :location, :seed_distribution, :other_info, :biome_id, :image, :comments
 end
