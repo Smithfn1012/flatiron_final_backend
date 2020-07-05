9c42972696a41c784d0758ccce0706a0a219ff04
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-05 16:14:52 -0500

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 Biome.destroy_all
 Animal.destroy_all
 Plant.destroy_all
